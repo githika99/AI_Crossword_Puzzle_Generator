e89df6a75680c540f94df82ca48a6df7540f5c14
Gavin Lebo <gavinlebo10@gmail.com>
2025-03-16 14:34:45 -0700

    added punkt_tab

--- nltk_setup.py
+++ nltk_setup.py
@@ -3,3 +3,4 @@ import nltk
 nltk.download('wordnet')       # wordnet database
 nltk.download('stopwords')     # stopword list
 nltk.download('punkt')         # tokenizing
+nltk.download('punkt_tab')
